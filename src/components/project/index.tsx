59fc0556fc16570ae7c0fa1d610879b9833e2aa2
TyperGuy <ray.developper@gmail.com>
2022-09-16 14:19:18 +0100

    feat : update project card UI

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -10,27 +10,10 @@ import {
   Title,
 } from "./Project.styles";
 import { FaGithubAlt } from "react-icons/fa";
-import { useState } from "react";
 
-const colors = [
-  "#00b9ae",
-  "#ea3546",
-  "#ef476f",
-  "#ff9500",
-  "#3a6ea5",
-  "#05668d",
-  "#ffadad",
-  "#0cf574",
-];
+
 
 const Project: React.FC = () => {
-  const [index, setIndex] = useState(0);
-  function getRandomColor(colors: string[]): number {
-    // get random index value from 0 to array.length-1
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    if (randomIndex === index) return getRandomColor(colors);
-    return randomIndex;
-  }
 
   return (
     <Container
